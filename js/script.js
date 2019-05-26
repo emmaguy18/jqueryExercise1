03e8ef4fbfb62362c26650a2c1dd8bb60521e150
Emma Guy <932400387@bun2-206-24.local>
2019-05-27 11:49:06 +1200

    tried using error function

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//Jquery code for if image link missing//
+// $(document).ready(function (){
+//   console.log("jquery has loaded");
+// });
+
+//error event handler//
+// $("#butterfly")
+//   .error(function(){
+//     alert("Handler for .error() called.")
+//   })
+//   .attr("src", "missing.jpg");
