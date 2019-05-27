4c590db49fb01c56f92a3b5185c19730247ffdba
Emma Guy <932400387@bun2-206-24.local>
2019-05-28 11:49:12 +1200

    added new jquery content

--- js/script.js
+++ js/script.js
@@ -9,3 +9,26 @@
 //     alert("Handler for .error() called.")
 //   })
 //   .attr("src", "missing.jpg");
+
+//replaceWith API//
+//replacing image with text//
+$("#butterfly").replaceWith("<h1>Butterfly</h1>");
+//replacing inmage with another image using id//
+//$("#butterfly").replaceWith("#DefaultImg");
+
+//blur event//
+$("#inputField").blur(function(){
+  console.log("this field is blurred")
+})
+
+//focus event//
+$("#inputField").focus(function(){
+  console.log("this field is focussed")
+})
+
+//val API//
+$("#myForm").submit(function(){
+  event.preventDefault();
+  var value= $("#inputField").val();
+  console.log("the value is " + value);
+})
